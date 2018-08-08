2730b7a8ff75018929178c60bcac2cf3b3d62911
Dmitry Ivanishkin <d@ivanishkin.ru>
2018-08-08 19:34:10 +0700

    COMP: hint appears above <> in method call for a moment

--- src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
+++ src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
@@ -8,48 +8,46 @@ package org.rust.ide.hints
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.lang.parameterInfo.*
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElement
+import com.twelvemonkeys.lang.StringUtil
 import org.rust.ide.utils.CallInfo
-import org.rust.lang.core.psi.RsCallExpr
-import org.rust.lang.core.psi.RsMethodCall
-import org.rust.lang.core.psi.RsTypeArgumentList
-import org.rust.lang.core.psi.RsValueArgumentList
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.ext.RsGenericDeclaration
 import org.rust.lang.core.psi.ext.ancestorStrict
 import org.rust.lang.core.types.type
 import org.rust.stdext.buildList
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import sun.swing.StringUIClientPropertyKey
+import kotlin.jvm.javaClass
 
-// RsArgumentsDescription from RsParameterInfoHandler
-class RsGenericParameterInfoHandler : ParameterInfoHandler<PsiElement, RsTypesDescription> {
+class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, RsTypeParameter> {
+    fun newLogger(): Logger = LoggerFactory.getLogger(javaClass)
+    val log = newLogger()
 
     var hintText: String = ""
 
-    override fun showParameterInfo(element: PsiElement, context: CreateParameterInfoContext) {
-        if (element !is RsTypeArgumentList) return
-        val valueArgList = element.parent.parent.nextSibling as? RsValueArgumentList ?: return
-        val argsDescr = RsTypesDescription.findDescriptionInExpr(valueArgList) ?: return
-        context.itemsToShow = arrayOf(argsDescr) //arrayOf(RsArgumentsDescription(arrayOf("Dummy hint")))
-        context.showHint(element, element.textRange.startOffset, this);
+    // done, todo: check offset
+    override fun showParameterInfo(element: RsTypeArgumentList, context: CreateParameterInfoContext) {
+        context.showHint(element, element.textRange.startOffset, this)
     }
 
-    override fun updateParameterInfo(parameterOwner: PsiElement, context: UpdateParameterInfoContext) {
+    //
+    override fun updateParameterInfo(parameterOwner: RsTypeArgumentList, context: UpdateParameterInfoContext) {
+        // val argIndex = findArgumentIndex
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun updateUI(p: RsTypesDescription?, context: ParameterInfoUIContext) {
+    override fun updateUI(p: RsTypeParameter, context: ParameterInfoUIContext) {
+        System.err.println("${p.name}")
+        System.err.println("${p.outerAttrList.map { it -> it.text }}")
+        System.err.println("${p.typeParamBounds?.text}") //polyboundList?.map {it -> it.bound.text}}")
+        hintText = p.name + p.outerAttrList.map { it -> it.text }
         // Bulb appears not strictly above the cursor sometimes
-        if (p == null) {
-            context.isUIComponentEnabled = false
-            return
-        }
-        val range = p.getArgumentRange(context.currentParameterIndex)
-        hintText = p.presentText
-        System.err.println(range) // dbg
-        System.err.println(range.startOffset.toString() + "," + range.endOffset) // dbg
-        p.arguments.asIterable().forEach({System.err.println(it)}) // dbg
+        val highlightEndOffset = hintText.length
         context.setupUIComponentPresentation(
             hintText,
-            range.startOffset,
-            range.endOffset,
+            0,
+            highlightEndOffset,
             false, // grayed  // !context.isUIComponentEnabled, ???
             false,
             false, // Define grayed part of args before highlight
@@ -57,76 +55,76 @@ class RsGenericParameterInfoHandler : ParameterInfoHandler<PsiElement, RsTypesDe
         )
     }
 
-    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    // ???
+    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? = null
 
-    override fun couldShowInLookup() = true
+    // ???
+    override fun couldShowInLookup() = false
 
+    // done
     override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext) =
-        context.file.findElementAt(context.editor.caretModel.offset)
+        context.file.findElementAt(context.editor.caretModel.offset) as? RsTypeArgumentList
 
     /**
      * From ParameterInfoHandler interface:
      * "Find element for parameter info should also set ItemsToShow in context and may set highlighted element"
-     * ???
-      */
+     */
 
-    override fun findElementForParameterInfo(context: CreateParameterInfoContext): PsiElement? {
-        val contextElement = context.file.findElementAt(context.editor.caretModel.offset) ?: return null
-        return findElementForParameterInfo(contextElement)
+    //
+    override fun findElementForParameterInfo(context: CreateParameterInfoContext): RsTypeArgumentList? {
+        val parameterList: RsTypeArgumentList? = context.file.findElementAt(context.editor.caretModel.offset)?.ancestorStrict()
+        if (parameterList is RsTypeArgumentList) {
+            val ancestor = parameterList.parent
+            System.err.println("parent: ${ancestor}")
+            System.err.println("parent.parent: ${ancestor.parent}")
+            System.err.println("parent.parent.parent: ${ancestor.parent.parent}")
+            when (ancestor) {
+                is RsMethodCall -> {
+                    val func = ancestor.reference.resolve() ?: return null
+                    val typeParams = (func as? RsGenericDeclaration)?.typeParameterList ?: return null
+                    val typeParamsArray = typeParams.typeParameterList.toTypedArray()
+                    context.itemsToShow = typeParamsArray
+                    System.err.println("MethodCall, done")
+                }
+                else -> {
+                    @Suppress("NAME_SHADOWING")
+                    val ancestor = ancestor.parent?.parent
+                    when (ancestor) {
+                        is RsCallExpr -> {
+                            System.err.println("is CallExpr")
+                        }
+                        is RsTypeReference -> {
+                            System.err.println("isTypeRef")
+                        }
+                        else -> {
+                            TODO("Not implemented, p.p.p: ${ancestor?.parent?.parent}")
+                        }
+                    }
+                }
+            }
+
+
+
+            return parameterList
+        }
+        return null
     }
+    /*private fun findArgumentIndex(place: PsiElement): Int {
+        val callArgs = place.ancestorStrict<RsValueArgumentList>() ?: return RsParameterInfoHandler.INVALID_INDEX
+        val descr = RsArgumentsDescription.findDescription(callArgs) ?: return RsParameterInfoHandler.INVALID_INDEX
+        var index = -1
+        if (descr.arguments.isNotEmpty()) {
+            index += generateSequence(callArgs.firstChild, { c -> c.nextSibling })
+                .filter { it.text == "," }
+                .count({ it.textRange.startOffset < place.textRange.startOffset }) + 1
+            if (index >= descr.arguments.size) {
+                index = -1
+            }
+        }
+        return index
+    }*/
 
     /**
-     * Returns RsTypeArgumentList, if it is a parent of contextElement, otherwise returns null
+     * Holds information about generic types
      */
-    private fun findElementForParameterInfo(contextElement: PsiElement) =
-        contextElement.ancestorStrict<RsTypeArgumentList>()
-}
-
-
-/**
- * Holds information about generic types
- */
-
-class RsTypesDescription(
-    val arguments: Array<String>
-) {
-    fun getArgumentRange(index: Int): TextRange {
-        if (index < 0 || index >= arguments.size) return TextRange.EMPTY_RANGE
-        val start = arguments.take(index).sumBy { it.length + 2 }
-        return TextRange(start, start + arguments[index].length)
-    }
-
-    val presentText = if (arguments.isEmpty()) "<no arguments>" else arguments.joinToString(", ")
-
-    companion object {
-        /**
-         * Finds declaration of the func/method and creates description of its arguments
-         */
-        fun findDescriptionInExpr(args: RsValueArgumentList): RsTypesDescription? {
-            args.exprList.forEach({ System.err.println(it.type)}) // dbg
-            val types = buildList<String> {
-                addAll(args.exprList.map { "${it.type}" })
-            }
-            return RsTypesDescription(types.toTypedArray())
-        }
-        fun findDescription(args: RsValueArgumentList): RsArgumentsDescription? {
-            // CALL_EXPR for fun or method with explicit &self || METHOD_CALL for method
-            val call = args.parent
-            val callInfo = when (call) {
-                is RsCallExpr -> CallInfo.resolve(call)
-                is RsMethodCall -> CallInfo.resolve(call)
-                else -> null
-            } ?: return null
-            val params = buildList<String> {
-                if (callInfo.selfParameter != null && call is RsCallExpr) {
-                    add(callInfo.selfParameter)
-                }
-                addAll(callInfo.parameters.map { "${it.pattern}: ${it.type}" })
-            }
-            // params.asIterable().forEach({ System.err.println(it)}) // dbg
-            return RsArgumentsDescription(params.toTypedArray())
-        }
-    }
 }

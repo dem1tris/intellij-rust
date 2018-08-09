1b300b29d644d8cf5520ac12f36644b70eab168c
Dmitry Ivanishkin <d@ivanishkin.ru>
2018-08-09 20:04:44 +0700

    COMP: hints works in function calls, dotted methods calls, types

--- src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
+++ src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
@@ -7,46 +7,68 @@ package org.rust.ide.hints
 
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.lang.parameterInfo.*
-import com.intellij.openapi.util.TextRange
-import com.twelvemonkeys.lang.StringUtil
-import org.rust.ide.utils.CallInfo
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.RsGenericDeclaration
 import org.rust.lang.core.psi.ext.ancestorStrict
-import org.rust.lang.core.types.type
-import org.rust.stdext.buildList
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import sun.swing.StringUIClientPropertyKey
-import kotlin.jvm.javaClass
+import org.rust.lang.core.psi.ext.typeParameters
 
-class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, RsTypeParameter> {
-    fun newLogger(): Logger = LoggerFactory.getLogger(javaClass)
-    val log = newLogger()
 
-    var hintText: String = ""
+class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, List<RsTypeParameter>> {
 
-    // done, todo: check offset
+    private var hintText: String = ""
+    private var highlightStartOffset = -1
+    private var highlightEndOffset = -1
+    private var curParam = -1
+
+    // todo: check offset
     override fun showParameterInfo(element: RsTypeArgumentList, context: CreateParameterInfoContext) {
         context.showHint(element, element.textRange.startOffset, this)
     }
 
-    //
+    // todo: check: cursor left than <, bulb still present without text
     override fun updateParameterInfo(parameterOwner: RsTypeArgumentList, context: UpdateParameterInfoContext) {
-        // val argIndex = findArgumentIndex
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // todo: fix: hint follows cursor even on lines below
+        curParam = ParameterInfoUtils.getCurrentParameterIndex(parameterOwner.node, context.offset, RsElementTypes.COMMA)
+        // todo: understand
+        when {
+            context.parameterOwner == null -> context.parameterOwner = parameterOwner
+            context.parameterOwner != parameterOwner -> context.removeHint()
+        }
     }
 
-    override fun updateUI(p: RsTypeParameter, context: ParameterInfoUIContext) {
-        System.err.println("${p.name}")
-        System.err.println("${p.outerAttrList.map { it -> it.text }}")
-        System.err.println("${p.typeParamBounds?.text}") //polyboundList?.map {it -> it.bound.text}}")
-        hintText = p.name + p.outerAttrList.map { it -> it.text }
-        // Bulb appears not strictly above the cursor sometimes
-        val highlightEndOffset = hintText.length
+    private fun ordinalIndexOrLength(str: String, substr: String, n: Int): Int {
+        var m = n
+        var pos = str.indexOf(substr)
+        while (--m > 0 && pos != -1)
+            pos = str.indexOf(substr, pos + 1)
+        return when (pos) {
+            -1 -> str.length
+            else -> pos
+        }
+    }
+
+    // todo: check: one-char hint isn't a bulb and hide text on the line
+    override fun updateUI(params: List<RsTypeParameter>, context: ParameterInfoUIContext) {
+        hintText = params.joinToString { it ->
+            if (it.name != null)
+                it.name + (it.typeParamBounds?.text ?: "")
+            else
+                ""
+        }
+        if (curParam >= 0 && curParam < params.size) {
+            highlightStartOffset = if (curParam == 0)
+                0
+            else
+                ordinalIndexOrLength(hintText, ",", curParam) + 1
+            highlightEndOffset = ordinalIndexOrLength(hintText, ",", curParam + 1)
+        } else {
+            highlightStartOffset = -1
+            highlightEndOffset = -1
+        }
+        // todo: Bulb appears not strictly above the cursor sometimes
         context.setupUIComponentPresentation(
             hintText,
-            0,
+            highlightStartOffset,
             highlightEndOffset,
             false, // grayed  // !context.isUIComponentEnabled, ???
             false,
@@ -55,76 +77,36 @@ class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, R
         )
     }
 
-    // ???
+    // done
     override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? = null
 
-    // ???
+    // done
     override fun couldShowInLookup() = false
 
     // done
-    override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext) =
-        context.file.findElementAt(context.editor.caretModel.offset) as? RsTypeArgumentList
-
-    /**
-     * From ParameterInfoHandler interface:
-     * "Find element for parameter info should also set ItemsToShow in context and may set highlighted element"
-     */
+    override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): RsTypeArgumentList? =
+        context.file.findElementAt(context.editor.caretModel.offset)?.ancestorStrict()
 
     //
     override fun findElementForParameterInfo(context: CreateParameterInfoContext): RsTypeArgumentList? {
         val parameterList: RsTypeArgumentList? = context.file.findElementAt(context.editor.caretModel.offset)?.ancestorStrict()
         if (parameterList is RsTypeArgumentList) {
-            val ancestor = parameterList.parent
-            System.err.println("parent: ${ancestor}")
-            System.err.println("parent.parent: ${ancestor.parent}")
-            System.err.println("parent.parent.parent: ${ancestor.parent.parent}")
-            when (ancestor) {
-                is RsMethodCall -> {
-                    val func = ancestor.reference.resolve() ?: return null
-                    val typeParams = (func as? RsGenericDeclaration)?.typeParameterList ?: return null
-                    val typeParamsArray = typeParams.typeParameterList.toTypedArray()
-                    context.itemsToShow = typeParamsArray
-                    System.err.println("MethodCall, done")
-                }
-                else -> {
-                    @Suppress("NAME_SHADOWING")
-                    val ancestor = ancestor.parent?.parent
-                    when (ancestor) {
-                        is RsCallExpr -> {
-                            System.err.println("is CallExpr")
-                        }
-                        is RsTypeReference -> {
-                            System.err.println("isTypeRef")
-                        }
-                        else -> {
-                            TODO("Not implemented, p.p.p: ${ancestor?.parent?.parent}")
-                        }
-                    }
-                }
+            val parent = parameterList.parent
+            val genericOwner = when (parent) {
+                is RsMethodCall -> parent
+                is RsPath -> parent.parent
+                else -> return null
             }
-
-
-
+            val genericDeclaration = when (genericOwner) {
+                is RsMethodCall -> genericOwner.reference.resolve()
+                is RsPathExpr -> genericOwner.path.reference.resolve()
+                is RsBaseType -> genericOwner.path?.reference?.resolve()
+                else -> return null
+            } as? RsGenericDeclaration ?: return null
+            val typesWithBounds = genericDeclaration.typeParameters
+            context.itemsToShow = arrayOf(typesWithBounds) // one-element array for one-line hint
             return parameterList
         }
         return null
     }
-    /*private fun findArgumentIndex(place: PsiElement): Int {
-        val callArgs = place.ancestorStrict<RsValueArgumentList>() ?: return RsParameterInfoHandler.INVALID_INDEX
-        val descr = RsArgumentsDescription.findDescription(callArgs) ?: return RsParameterInfoHandler.INVALID_INDEX
-        var index = -1
-        if (descr.arguments.isNotEmpty()) {
-            index += generateSequence(callArgs.firstChild, { c -> c.nextSibling })
-                .filter { it.text == "," }
-                .count({ it.textRange.startOffset < place.textRange.startOffset }) + 1
-            if (index >= descr.arguments.size) {
-                index = -1
-            }
-        }
-        return index
-    }*/
-
-    /**
-     * Holds information about generic types
-     */
 }

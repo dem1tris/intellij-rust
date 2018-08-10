915649d187ea5c52a26577b46fc0a32b5f852271
Dmitry Ivanishkin <d@ivanishkin.ru>
2018-08-10 16:58:59 +0700

    COMP: type-parameter hints in types, method calls and function calls

--- src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
+++ src/main/kotlin/org/rust/ide/hints/RsGenericParameterInfoHandler.kt
@@ -7,106 +7,104 @@ package org.rust.ide.hints
 
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.lang.parameterInfo.*
+import com.intellij.openapi.util.TextRange
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.RsGenericDeclaration
 import org.rust.lang.core.psi.ext.ancestorStrict
+import org.rust.lang.core.psi.ext.elementType
 import org.rust.lang.core.psi.ext.typeParameters
 
+class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, RsGenericPresentation> {
 
-class RsGenericParameterInfoHandler : ParameterInfoHandler<RsTypeArgumentList, List<RsTypeParameter>> {
-
-    private var hintText: String = ""
-    private var highlightStartOffset = -1
-    private var highlightEndOffset = -1
     private var curParam = -1
 
-    // todo: check offset
     override fun showParameterInfo(element: RsTypeArgumentList, context: CreateParameterInfoContext) {
+        context.highlightedElement = null
         context.showHint(element, element.textRange.startOffset, this)
     }
 
-    // todo: check: cursor left than <, bulb still present without text
+    // todo: check: cursor moving on around <, bulb still present without text, IDEA-197133
+    // todo: don't disappear in nested generic types
     override fun updateParameterInfo(parameterOwner: RsTypeArgumentList, context: UpdateParameterInfoContext) {
-        // todo: fix: hint follows cursor even on lines below
         curParam = ParameterInfoUtils.getCurrentParameterIndex(parameterOwner.node, context.offset, RsElementTypes.COMMA)
-        // todo: understand
         when {
+            // todo: understand
             context.parameterOwner == null -> context.parameterOwner = parameterOwner
+
+            // occurs in case of up-down cursor moving
             context.parameterOwner != parameterOwner -> context.removeHint()
         }
     }
 
-    private fun ordinalIndexOrLength(str: String, substr: String, n: Int): Int {
-        var m = n
-        var pos = str.indexOf(substr)
-        while (--m > 0 && pos != -1)
-            pos = str.indexOf(substr, pos + 1)
-        return when (pos) {
-            -1 -> str.length
-            else -> pos
-        }
-    }
-
-    // todo: check: one-char hint isn't a bulb and hide text on the line
-    override fun updateUI(params: List<RsTypeParameter>, context: ParameterInfoUIContext) {
-        hintText = params.joinToString { it ->
-            if (it.name != null)
-                it.name + (it.typeParamBounds?.text ?: "")
-            else
-                ""
-        }
-        if (curParam >= 0 && curParam < params.size) {
-            highlightStartOffset = if (curParam == 0)
-                0
-            else
-                ordinalIndexOrLength(hintText, ",", curParam) + 1
-            highlightEndOffset = ordinalIndexOrLength(hintText, ",", curParam + 1)
-        } else {
-            highlightStartOffset = -1
-            highlightEndOffset = -1
-        }
-        // todo: Bulb appears not strictly above the cursor sometimes
+    override fun updateUI(p: RsGenericPresentation, context: ParameterInfoUIContext) {
+        context.currentParameterIndex
         context.setupUIComponentPresentation(
-            hintText,
-            highlightStartOffset,
-            highlightEndOffset,
-            false, // grayed  // !context.isUIComponentEnabled, ???
+            p.presentText,
+            p.getRange(curParam).startOffset,
+            p.getRange(curParam).endOffset,
+            false, // grayed
             false,
-            false, // Define grayed part of args before highlight
+            false, // define grayed part of args before highlight
             context.defaultParameterColor
         )
     }
 
-    // done
     override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? = null
 
-    // done
     override fun couldShowInLookup() = false
 
-    // done
     override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): RsTypeArgumentList? =
-        context.file.findElementAt(context.editor.caretModel.offset)?.ancestorStrict()
+        findExceptColonColon(context)
 
-    //
     override fun findElementForParameterInfo(context: CreateParameterInfoContext): RsTypeArgumentList? {
-        val parameterList: RsTypeArgumentList? = context.file.findElementAt(context.editor.caretModel.offset)?.ancestorStrict()
-        if (parameterList is RsTypeArgumentList) {
-            val parent = parameterList.parent
-            val genericOwner = when (parent) {
-                is RsMethodCall -> parent
-                is RsPath -> parent.parent
-                else -> return null
-            }
-            val genericDeclaration = when (genericOwner) {
-                is RsMethodCall -> genericOwner.reference.resolve()
-                is RsPathExpr -> genericOwner.path.reference.resolve()
-                is RsBaseType -> genericOwner.path?.reference?.resolve()
-                else -> return null
-            } as? RsGenericDeclaration ?: return null
-            val typesWithBounds = genericDeclaration.typeParameters
-            context.itemsToShow = arrayOf(typesWithBounds) // one-element array for one-line hint
-            return parameterList
+        val parameterList = findExceptColonColon(context) ?: return null
+        val parent = parameterList.parent
+        val genericDeclaration = when (parent) {
+            is RsMethodCall -> parent.reference.resolve()
+            is RsPath -> parent.reference.resolve()
+            else -> return null
+        } as? RsGenericDeclaration ?: return null
+        val typesWithBounds = genericDeclaration.typeParameters
+        // one-element array for one-line hint
+        context.itemsToShow = arrayOf(RsGenericPresentation(typesWithBounds))
+        return parameterList
+    }
+
+    private fun findExceptColonColon(context: ParameterInfoContext?): RsTypeArgumentList? {
+        val element = context?.file?.findElementAt(context.editor.caretModel.offset) ?: return null
+        if (element.elementType == RsElementTypes.COLONCOLON) return null
+        return element.ancestorStrict() ?: return null
+    }
+}
+
+class RsGenericPresentation(
+    private val params: List<RsTypeParameter>
+) {
+    // what if they all without names?
+    val presentText = params.joinToString { it ->
+        if (it.name != null)
+            it.name + (it.typeParamBounds?.text ?: "")
+        else
+            ""
+    }
+
+    fun getRange(index: Int): TextRange {
+        return if (index < 0 || index >= params.size)
+            TextRange.EMPTY_RANGE
+        else
+            ranges[index]
+    }
+
+    private val ranges: List<TextRange> = params.indices.map { it ->
+        calculateRange(it)
+    }
+
+    private fun calculateRange(index: Int): TextRange {
+        val start = params.take(index).sumBy {
+            it.name?.length?.plus(it.typeParamBounds?.text?.length ?: 0)
+                ?.plus(2) ?: 0 // means plus ", "
         }
-        return null
+        return TextRange(start, start.plus(params[index].name?.length ?: 0)
+            .plus(params[index].typeParamBounds?.text?.length ?: 0))
     }
 }

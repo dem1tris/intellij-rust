c47a3411a52a538ff2811b9d9904e4918646e18a
Dmitry Ivanishkin <d@ivanishkin.ru>
2018-08-21 19:09:34 +0700

    COMP: contributor created

---
+++ src/main/kotlin/org/rust/lang/core/completion/RsSmartCompletionContributor.kt
@@ -0,0 +1,16 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.lang.core.completion
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionType
+
+class RsSmartCompletionContributor : CompletionContributor() {
+
+    init {
+    }
+
+}
